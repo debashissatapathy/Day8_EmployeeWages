947f6cec58587db7539dd1d2b7ec5e6cf1128b94
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-26 16:13:36 +0530

    Fetched

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            //UC1_Attendance p = new UC1_Attendance();
+            //p.Emp_Attendance();
+            //UC2_DailyEmpWages wage = new UC2_DailyEmpWages();
+            //wage.Emp_Wages();
+            //UC3_Emp_PartTime part = new UC3_Emp_PartTime();
+            //part.Emp_PartTime();
+            //UC4_SwitchCase emp =new UC4_SwitchCase();
+            //emp.Emp_Swichcase();
+            //UC5_MonthlyWages month = new UC5_MonthlyWages();
+            //month.Emp_MontlyWages();
+            UC6_Condition code = new UC6_Condition();
+            code.Emp_Condition();
+        }
+    }
+}
