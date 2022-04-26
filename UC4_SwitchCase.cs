a7080bbd6e4ae16b5f160134f17dfc697eced3e0
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-26 16:03:05 +0530

    Fetched

---
+++ UC4_SwitchCase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWages
+{
+    class UC4_SwitchCase
+    {
+        public void Emp_Swichcase()
+        {
+            int WagePerHr = 20, FullDayHr = 8;
+            int DailyWage = WagePerHr * FullDayHr;
+            Console.WriteLine("\nEnter vlaue 0 for PartTime and 1 for FullTime");
+            int isPartTime = int.Parse(Console.ReadLine());
+            switch(isPartTime)
+            {
+                case 0:
+                    Console.WriteLine("PartTime employee daily wage is: " + DailyWage/2);
+                    break;
+                case 1:
+                    Console.WriteLine("FullTime employee daily wage is: " + DailyWage);
+                    break;
+            }
+            
+                
+
+
+        }
+    }
+}
