6463269cef281f39961d049250fffe55c35ea04b
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-04-26 16:01:08 +0530

    Fetched

---
+++ UC3_Emp_PartTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWages
+{
+    class UC3_Emp_PartTime
+    {
+        public void Emp_PartTime()
+        {
+            int WagePerHr = 20, FullDayHr = 8;
+            int DailyWage = WagePerHr * FullDayHr;
+            Console.WriteLine("Enter vlaue 0 for PartTime and 1 for FullTime");
+            int isPartTime = int.Parse(Console.ReadLine());
+            if(isPartTime==0)
+            {
+                Console.WriteLine("PartTime employee daily wage is: " + DailyWage/2);
+            }
+            else
+                Console.WriteLine("FullTime employee daily wage is: " + DailyWage);
+
+
+        }
+    }
+}
